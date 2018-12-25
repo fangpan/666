3d121487aaca75dc4e19ec7b682ea053635f9c58
fangpan <31238619+fangpan@users.noreply.github.com>
2018-12-25 10:59:38 +0800

    Add files via upload

---
+++ map/pages/community/community.js
@@ -0,0 +1,132 @@
+Page({
+  data: {
+     markers: [],
+     showMore: false,
+  },
+
+  onReady: function (e) {
+     // 使用 wx.createMapContext 获取 map 上下文 
+     var  pathURL = getApp().globalData.PATH
+     this.mapCtx = wx.createMapContext('map');
+     this.setData({
+          pathURL: pathURL
+    })
+  },
+//展示品牌
+  showLogoModel:function(){
+       //util.showLogoModel()
+       //app.showLogoModel()
+       wx.showModal({
+          title: '公众号搜索 "游品慧" ',
+          content: '公众号搜索 "游品慧" 并添加关注，探寻地质地理和大自然的奥秘，了解科普实践课程情况',
+          showCancel: false, //不显示取消按钮
+          confirmText: '确定'
+        })
+  },
+  onLoad: function (options) {
+    var that=this
+    var id = options.id;
+    var  pathURL = getApp().globalData.PATH
+    //获取社区大学详情
+    wx.request({
+        url:""+pathURL+"/wx/scienceuniversity/info/"+id+"",
+        method:"GET",
+        header: {
+         'content-type': 'application/json'
+        },
+        success:function(res){
+            var list = res.data.scienceUniversity; 
+              list.callout.display = "ALWAYS";
+              list.callout.color = "#FF0000";
+              list.callout.padding = 4;
+              list.callout.content = list.name;
+              var unitName = list.name;   
+              console.log(list)
+            that.setData({
+                "markers[0]" : list,
+                 name : unitName
+            });
+            var name = list.name  
+           wx.setNavigationBarTitle({
+               title:name
+           })
+        }
+      });
+  //课程列表
+     wx.request({
+        url:""+pathURL+"/wx/curriculum/list?limit=9999&&id="+id+"",
+        method:"GET",
+        header: {
+         'content-type': 'application/json'
+        },
+        success:function(res){
+            var list = res.data.page.list;       
+           that.setData({
+                courseList : list
+            });
+            console.log(list)
+        }
+      });
+  },
+
+// 地图导航
+ markertap:function() {
+    var name = this.data.markers[0].name
+    var latitude = Number(this.data.markers[0].latitude)
+    var longitude = Number(this.data.markers[0].longitude)
+    wx.openLocation({
+      latitude: latitude,
+      longitude: longitude,
+      scale: 18,
+      name: name,
+      address:''
+    })
+ },
+
+//跳转到展教联盟
+ resources: function () {
+    wx.navigateTo({
+      url:'/pages/resources/resources'
+    })
+  },
+
+//跳转到特色场馆
+ changguan: function () {
+    wx.navigateTo({
+      url:'/pages/changguan/changguan'
+    })
+  },
+
+  //跳转到资源信息列表
+ newinfor: function () {
+    wx.navigateTo({
+      url:'/pages/newinfor/newinfor'
+    })
+  },
+
+ //跳转到个人中心
+ myself: function () {
+    wx.navigateTo({
+      url:'/pages/myself/myself'
+    })
+  },
+
+//查看更多
+listToggle: function () {
+    this.setData({
+      showMore: !this.data.showMore
+    })
+  },
+
+//跳转课程详情
+TOdetail: function (e) {
+    var id = e.currentTarget.id
+     wx.navigateTo({
+         url: '../course/course?id='+id+''
+       });
+  }
+
+})
+
+
+
---
+++ map/pages/course/course.js
@@ -0,0 +1,142 @@
+
+Page({
+  data: {
+    name:"",
+    imgUrls: [],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    audio:"",
+    video:"",
+  },
+ onReady: function (e) {
+      this.reload();
+      var  pathURL = getApp().globalData.PATH
+      this.setData({
+        pathURL: pathURL
+    })
+  },
+//音频播放
+ actionEvent: function(e) {
+    var method = this.data.status === 'play' ? 'pause' : 'play';
+    this.setData({
+      status: method,
+      action: {
+        method: method
+      }
+    });
+  },
+  timeupdateEvent: function(e) {
+    var t = e.detail.currentTime
+    this.setData({
+      timeText: this.formatTime(t),
+    });
+  },
+  endEvent: function() {
+     this.reload();
+  },
+  reload: function() {
+    this.setData({
+      per: 0,
+      status: 'pause',
+      currentTime: '0',
+      timeText: '00:00',
+    });
+    setTimeout(() => {
+      this.setData({
+        action: {
+          method: 'pause'
+        }
+      })
+    }, 100);
+  },
+  formatTime: function(time) {
+    time = Math.floor(time);
+    var m = Math.floor(time / 60).toString();
+    m = m.length < 2 ? '0' + m : m;
+    var s = (time - parseInt(m) * 60).toString();
+    s = s.length < 2 ? '0' + s : s;
+    return `${m}:${s}`;
+  },
+  
+  onLoad: function (options) {
+    var that = this
+    var id = options.id;
+    var pathURL = getApp().globalData.PATH
+   //课程详情
+   wx.request({
+        url:""+pathURL+"/wx/curriculum/info/"+id+"",
+        method:"GET",
+        header: {
+         'content-type': 'application/json'
+        },
+        success:function(res){
+            var list = res.data.curriculum;    
+            var pics = list.pictures
+            console.log(list)
+            if(pics!=null){
+              pics = pics.split(",")
+              for(var i = 0; i < pics.length; i++) {
+                  if(pics[i] == undefined || pics[i] == "") {
+                     pics.splice(i,1);
+                     i = i - 1; 
+                   }
+                }
+            }
+           that.setData({
+                 imgUrls:pics ,
+                 courseIntroduction:list.courseIntroduction,
+                 time:list.time ,
+                 lecturer:list.lecturer,
+                 telephone:list.telephone ,
+                 name:list.name,
+                 audio:list.audio,
+                 video:list.video
+            });
+           var name = that.data.name  
+           wx.setNavigationBarTitle({
+               title:name
+           })
+        }
+      });
+  },
+ 
+//拨打电话
+ calling: function (e) {
+    var NUM = e.currentTarget.dataset.num
+    console.log(NUM)
+    wx.makePhoneCall({
+        phoneNumber:NUM 
+      })
+  },
+
+//跳转到展教联盟
+ resources: function () {
+    wx.navigateTo({
+      url:'/pages/resources/resources'
+    })
+  },
+
+//跳转到特色场馆
+ changguan: function () {
+    wx.navigateTo({
+      url:'/pages/changguan/changguan'
+    })
+  },
+
+ //跳转到资源信息列表
+ newinfor: function () {
+    wx.navigateTo({
+      url:'/pages/newinfor/newinfor'
+    })
+  },
+
+ //跳转到个人中心
+ myself: function () {
+    wx.navigateTo({
+      url:'/pages/myself/myself'
+    })
+  }
+
+})
